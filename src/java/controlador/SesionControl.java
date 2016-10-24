83cc830efb05856c1cd9422c157939fd83d7b0c2
shelo25 <shelo.barraza@gmail.com>
2016-10-24 20:50:21 -0300

    mantenedoressss

---
+++ src/java/controlador/SesionControl.java
@@ -0,0 +1,12 @@
+package controlador;
+
+import POJO.Usuario;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ViewScoped
+@ManagedBean
+public class SesionControl {
+    private Usuario usuario;
+    
+}
